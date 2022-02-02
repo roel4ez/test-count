600f712fef2759cc58cb59fe3c937c908ad5f895
Roel Fauconnier <roel.fauconnier@microsoft.com>
2022-02-02 20:16:48 +0100

    Commit the script with a simple README

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # test-count
-Count number of tests per type in a git repository
+
+> Count number of tests per type in a git repository
+
+Simple PowerShell script that outputs a csv file of the number of tests, per
+type and per commit. This data can then be used to generate a simple chart such
+as:
+
+![Test Count Chart](chart.png)
+
+The script has a few parameters that can be set:
+
+> The script does not yet accept these as input parameters. These need to be set
+in the script itself!
+
+| Name | Default | Description |
+| --- | --- | --- |
+| `gitRepoPath` | | The path to the repository |
+| `e2ePath` | `.\Tests\E2E` | Path to the subdirectory where the e2e tests live |
+| `integrationPath` | `.\Tests\Integration` | Path to the subdirectory where the integration tests live |
+| `unitPath` | `.\Tests\Unit` | Path to the subdirectory where the unit tests live |
+| `testAttributeRegex` | `\[ *(Fact\|Theory) *\]` | The regular expression used to find the tests. This is usually the attribute which decorates your tests cases. Default value is for xUnit. |
+| `outputFile` | `test-count-csv` | The output csv file, will be stored in the current directory |
---
+++ test-count.ps1
@@ -0,0 +1,39 @@
+$e2ePath = "Tests/E2E"
+$integrationPath = "Tests/Integration" 
+$unitPath = "Tests/Unit"
+
+$testAttributeRegex = "\[ *(Fact|RetryFact|Theory) *\]"
+
+$outputFile = "test-count.csv"
+$fullOutputPath = Join-Path -Path $PWD -ChildPath $outputFile
+$gitRepoPath = "C:\sources\lora\iotedge-lorawan"
+
+Write-Host "Current directory: " $PWD " OutputPath: " $fullOutputPath
+Clear-Content -Path $fullOutputPath -ErrorAction SilentlyContinue
+Push-Location $gitRepoPath
+Write-Host "Changed to repository directory: " $PWD
+
+Write-Host "Counting E2E tests..."
+
+git log '--pretty=%h %ai' $e2ePath |
+  ForEach-Object { $t = $_ -split ' ', 4;
+      New-Object psobject -Property @{ Type = "E2E"; Commit = $t[0]; Date = $t[1]; TestCount = $(git grep -E $testAttributeRegex $t[0] -- $e2ePath\*.cs  | Measure-Object | Select-Object -ExpandProperty Count) }
+  } | ConvertTo-Csv | Out-File -Path $fullOutputPath
+
+Write-Host "Counting Integration tests..."
+
+git log '--pretty=%h %ai' $integrationPath |
+  ForEach-Object { $t = $_ -split ' ', 4;
+      New-Object psobject -Property @{ Type = "Integration"; Commit = $t[0]; Date = $t[1]; TestCount = $(git grep -E $testAttributeRegex $t[0] -- $e2ePath\*.cs  | Measure-Object | Select-Object -ExpandProperty Count) }
+  } | ConvertTo-Csv | Select-Object -Skip 1 | Out-File -Append -Path $fullOutputPath
+
+Write-Host "Counting Unit tests..."
+
+git log '--pretty=%h %ai' $unitPath |
+  ForEach-Object { $t = $_ -split ' ', 4;
+      New-Object psobject -Property @{ Type = "Unit"; Commit = $t[0]; Date = $t[1]; TestCount = $(git grep -E $testAttributeRegex $t[0] -- $e2ePath\*.cs  | Measure-Object | Select-Object -ExpandProperty Count) }
+  } | ConvertTo-Csv | Select-Object -Skip 1 | Out-File -Append -Path $fullOutputPath
+
+Pop-Location
+
+Write-Host "Done, Curent Directory: " + $PWD
